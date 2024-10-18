898bf798f3d7cadcedd5a5cccf2e02b03b111ab2
teddy <teddystwn48@gmail.com>
2024-10-18 11:23:01 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
     <div className="flex w-full h-full p-10 justify-center items-center flex-row flex-wrap">
       <div className="flex max-sm:w-auto flex-col border-gray-300 border-2 px-0 rounded-lg flex-wrap py-0 h-full items-center justify-center bg-slate-50">
         <span className="flex p-2 w-full italic text-justify">
-          "{data ? data[0]?.content : ""}"
+        &quot;{data ? data[0]?.content : ""}"
         </span>
         <span className="flex p-2 w-full">
           <span className="flex justify-start w-full">
